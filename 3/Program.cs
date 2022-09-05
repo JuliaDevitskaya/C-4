9ee8aa9a21b2efb86d10a4c6caba8dbff515c398
Julia <yagubetslulka@yandex.ru>
2022-09-05 13:11:35 +0300

    Задача3. Массив из 8 элементов

---
+++ 3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+using static System.Console;
+Clear();
+
+int[] Array=new int [8];
+FillArr(Array);
+WriteLine($"[{String.Join(",",Array)}]");
+
+void FillArr (int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i]=new Random().Next(0,20);
+    }
+}
